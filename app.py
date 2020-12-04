81fa5a581e1270c88580e5f40e8024cb09f7d6de
sree-chikati <srechi453.fusdk12@gmail.com>
2020-12-03 21:37:14 -0800

    Challange 6: Delete Plant

--- app.py
+++ app.py
@@ -125,9 +125,11 @@ def edit(plant_id):
 def delete(plant_id):
     # TODO: Make a `delete_one` database call to delete the plant with the given
     # id.
+    delete_plant = mongo.db.plants.delete_one({'_id': ObjectId(plant_id)})
 
     # TODO: Also, make a `delete_many` database call to delete all harvests with
     # the given plant id.
+    delete_harvest = mongo.db.harvests.delete_many({'plant_id': plant_id})
 
     return redirect(url_for('plants_list'))
 
