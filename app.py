7c511dcebf901eb46184308adebe0dfd77de77d8
sree-chikati <srechi453.fusdk12@gmail.com>
2020-12-03 21:34:37 -0800

    Challange 5: Edit Plant

--- app.py
+++ app.py
@@ -100,13 +100,20 @@ def edit(plant_id):
     if request.method == 'POST':
         # TODO: Make an `update_one` database call to update the plant with the
         # given id. Make sure to put the updated fields in the `$set` object.
-
+        plant_update = mongo.db.plants.update_one({'_id': ObjectId(plant_id)},
+        {
+            '$set': {
+                'name': request.form.get('plant_name'),
+                'variety': request.form.get('variety'),
+                'photo_url': request.form.get('photo'),
+                'date_planted': request.form.get('date_planted')}
+        })
         
         return redirect(url_for('detail', plant_id=plant_id))
     else:
         # TODO: Make a `find_one` database call to get the plant object with the
         # passed-in _id.
-        plant_to_show = ''
+        plant_to_show = mongo.db.plants.find_one({'_id': ObjectId(plant_id)})
 
         context = {
             'plant': plant_to_show
